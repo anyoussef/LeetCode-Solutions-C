3f08ce73331e2f24e300d3b9d0d911bfa215cde7
anyoussef <tonerbtw@gmail.com>
2025-01-29 22:46:59 -0600

    Solution to 190. Reverse Bits

--- 190.c
+++ 190.c
@@ -1,5 +1,11 @@
-#include <ctype.h>
+#include <stdint.h>
 
 uint32_t reverseBits(uint32_t n) {
-
+    uint32_t result = 0;
+    for (int i = 0; i < 32; i++) {
+        result <<= 1;
+        result |= (n & 1);
+        n >>= 1;
+    }
+    return result;
 }
